ef4fccbdc9ed0fa90142b95fe062203d4fc998f1
NelsonKCT <104752032+NelsonKCT@users.noreply.github.com>
2023-06-20 11:38:06 +0800

    Change to gridmovement

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,32 +4,19 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private float horizontal;
-    private float speed = 8f;
-    private float jumpingPower = 16f;
-    [SerializeField] private Rigidbody2D rb;
-    [SerializeField] private Transform groundCheck;
-    [SerializeField] private LayerMask groundLayer;
+    public float horizontal;
+    public float moveSpeed = 5f;
+    public Transform movePoint; 
 
-    // Update is called once per frame
-    void Update()
-    {
+    void Start(){
+        movePoint.parent = null;
+    }
+    void Update(){
+        transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
         horizontal = Input.GetAxisRaw("Horizontal");
-        if(Input.GetButtonDown("Jump") && IsGrounded()){
-            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
-        }
-        if(Input.GetButtonUp("Jump") && rb.velocity.y > 0f){
-            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
-        }
-    }
-    private void FixedUpdate()
-    {
         if(horizontal < 0) horizontal = 0;
-        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
-        Debug.Log("rb.velocity : " + rb.velocity);
+        else if(horizontal == 1){
+            movePoint.position += new Vector3(horizontal, 0f, 0f);
+        }
     }
-    private bool IsGrounded(){
-        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
     }
-    
-}
