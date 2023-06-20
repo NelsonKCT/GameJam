1daa7f47dda96cdf2d442d63803839c066ba9b09
1101404 <puma8807@gmail.com>
2023-06-20 15:32:36 +0800

    腳色，Portal Update

---
+++ Assets/Scripts/Portal.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Portal : MonoBehaviour
+{
+    [SerializeField] private string nextSeceneName;
+
+    // Update is called once per frame
+
+}
