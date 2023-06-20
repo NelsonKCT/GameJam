4a7c70f83d2730355d5f993d668c7376196ec266
1101404 <puma8807@gmail.com>
2023-06-20 13:09:26 +0800

    PlayerMovement Update2

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,53 +6,111 @@ public class PlayerMovement : MonoBehaviour
 {
     private float movePos = 1f;
     private float moveTimeCount;
-    private float moveTime = 1f;
+    private float moveTime = 0.5f;
+    private bool isMoveFinished;
+    private bool isEnterPressed;
+    private bool moveForwardFinished;
 
-    private Queue playerInput = new Queue();
+    private Queue playerInputQueue = new Queue();
+    private Stack playerBackwardStack = new Stack();
     private int playerInputCount = 0;
+    private int playerBackwardCount = 0;
 
     void Start()
     {
         playerInputCount = 0;
+        isMoveFinished = true;
+        isEnterPressed = false;
+        moveForwardFinished = true;
     }
     void Update()
-    {   
+    {
+        Debug.Log(playerInputQueue.Count);
+
         moveTimeCount -= Time.deltaTime;
-        if(moveTimeCount < 0)
+        if (moveTimeCount < 0)
         {
             moveTimeCount = moveTime;
-            Debug.Log("Check");
+
+            if (playerInputCount > 0 && isEnterPressed)
+            {
+                PlayerMoveForward();
+            }
+            if (playerBackwardCount > 0 && moveForwardFinished)
+            {
+                PlayerMoveBackward();
+            }
         }
 
-        if(Input.GetKeyDown(KeyCode.D))
+        if (Input.GetKeyDown(KeyCode.D))
         {
-            playerInput.Enqueue("Left");
+            playerInputQueue.Enqueue("Left");
             playerInputCount++;
-            //transform.position = new Vector3(transform.position.x + movePos, transform.position.y, transform.position.z);
         }
         if (Input.GetKeyDown(KeyCode.W))
         {
-            playerInput.Enqueue("Up");
+            playerInputQueue.Enqueue("Up");
             playerInputCount++;
-            //transform.position = new Vector3(transform.position.x, transform.position.y + movePos, transform.position.z);
         }
-        if (Input.GetKeyDown(KeyCode.Return))
+        if (Input.GetKeyDown(KeyCode.Return) && isMoveFinished)
         {
-            while(playerInputCount > 0)
-            {
-                string input = (string)playerInput.Dequeue();
-                playerInputCount--;
-                
-                if(input == "Left")
-                {
-                    transform.position = new Vector3(transform.position.x + movePos, transform.position.y, transform.position.z);
-                }
-                else if(input == "Up")
-                {
-                    transform.position = new Vector3(transform.position.x, transform.position.y + movePos, transform.position.z);
-                }
-            }
-            //transform.position = new Vector3(transform.position.x, transform.position.y - movePos, transform.position.z);
+            isEnterPressed = true;
+            isMoveFinished = false;
+            moveForwardFinished = false;
+        }
+    }
+
+    private void PlayerMoveForward()
+    {
+        Debug.Log("Move");
+        string input = (string)playerInputQueue.Dequeue();
+        playerInputCount--;
+
+        if (input == "Left")
+        {
+            transform.position = new Vector3(transform.position.x + movePos, transform.position.y, transform.position.z);
+            playerBackwardStack.Push("Right");
+            playerBackwardCount++;
+        }
+        else if (input == "Up")
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y + movePos, transform.position.z);
+            playerBackwardStack.Push("Down");
+            playerBackwardCount++;
+        }
+
+        if (playerInputCount == 0)
+        {
+            StartCoroutine(WaitForMoveFinish());
+        }
+    }
+
+    IEnumerator WaitForMoveFinish()
+    {
+        yield return new WaitForSeconds(1f);
+        moveForwardFinished = true;
+        isEnterPressed = false;
+    }
+
+    private void PlayerMoveBackward()
+    {
+        Debug.Log("MoveBack");
+        string input = (string)playerBackwardStack.Pop();
+        playerBackwardCount--;
+
+        if (input == "Right")
+        {
+            transform.position = new Vector3(transform.position.x - movePos, transform.position.y, transform.position.z);
+        }
+        else if (input == "Down")
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y - movePos, transform.position.z);
+        }
+
+        if (playerBackwardCount == 0)
+        {
+            isMoveFinished = true;
+
         }
     }
 }
