715a9de0a9cbe6764af2d6da160e5656007a1901
1101404 <puma8807@gmail.com>
2023-06-21 18:57:08 +0800

    Reset Commit

---
+++ Assets/Scripts/Reset.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Reset : MonoBehaviour
+{
+    private Button reset;
+    [SerializeField] private string sceneName;
+    private void Start()
+    {
+        reset = transform.gameObject.GetComponent<Button>();
+    }
+
+    private void Update()
+    {
+        reset.onClick.AddListener(OnClick);
+    }
+
+    private void OnClick()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
