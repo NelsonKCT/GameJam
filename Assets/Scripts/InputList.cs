9b8068e7aee2f23ce4ac9359f9fbd5f1a84a6994
Noookia <matchman7980@gmail.com>
2023-06-21 18:41:29 +0800

    input list complete

--- Assets/Scripts/InputList.cs
+++ Assets/Scripts/InputList.cs
@@ -29,10 +29,10 @@ public class InputList : MonoBehaviour
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.Space)){
-            new_input =Instantiate(input_obj, transform.position + new Vector3((-input_n)*0.75f,0,0), transform.rotation);
+            new_input =Instantiate(input_obj, transform.position + new Vector3((input_n)*0.75f,0,0), transform.rotation);
             inputs.Add(new_input);
 
-            new_input = Instantiate(rvs_input_obj, transform.position + new Vector3((-input_n)*0.75f,-0.75f,0), transform.rotation);
+            new_input = Instantiate(rvs_input_obj, transform.position + new Vector3((input_n)*0.75f,-0.75f,0), transform.rotation);
             rvs_inputs.Add(new_input);
 
             input_n++;
@@ -62,8 +62,8 @@ public class InputList : MonoBehaviour
                 list_index--;
             }
             else if(list_index>=0 && list_index<=input_n && delRvs){
-                Destroy(rvs_inputs[0]);
-                rvs_inputs.Remove(rvs_inputs[0]);
+                Destroy(rvs_inputs[rvs_inputs.Count-1]);
+                rvs_inputs.Remove(rvs_inputs[rvs_inputs.Count-1]);
                 list_index--;
             }
             PM.doDelete=false;
